bcf7a22ce555365ac2324e71a21e69576b6c3ce4
Rien <rien@hnry.io>
2024-12-04 16:40:30 +1100

    golf

--- day 3/day3.rb
+++ day 3/day3.rb
@@ -1,17 +1,28 @@
 # data = File.read("day3.test.input")
-data = File.read("day3.input")
+# data = File.read("day3.input")
 
-def mul(str)
-  m = str.match(/mul\((\d+),(\d+)\)/)
-  m[1].to_i * m[2].to_i
-end
+# def mul(str)
+#   m = str.match(/mul\((\d+),(\d+)\)/)
+#   m[1].to_i * m[2].to_i
+# end
 
-re = /mul\(\d+,\d+\)/
+# re = /mul\(\d+,\d+\)/
 
-sum = 0
-data.scan(re).each do |m|
-  pp "-- #{m}"
-  sum += mul(m)
-end
+# sum = 0
+# data.scan(re).each do |m|
+#   pp "-- #{m}"
+#   sum += mul(m)
+# end
 
-pp "sum: #{sum}"
+# pp "sum: #{sum}"
+
+# p File.read(ARGV[0]).scan(/mul\(\d+,\d+\)/).sum{_1.match(/mul\((\d+),(\d+)\)/);$1.to_i*$2.to_i}
+# p File.read(ARGV[0]).scan(/mul\((\d+),(\d+)\)/).sum{_1.map(&:to_i).inject(&:*)}
+# p$<.read.scan(/mul\((\d+),(\d+)\)/).sum{_1.map(&:to_i).inject(&:*)}
+p$<.read.scan(/mul\((\d+),(\d+)\)/).sum{eval(_1*?*)}
+
+# `p` does not need a space
+# `$<.read` reads from a file
+# `_1` is first arg for `sum`
+# `*?*` joins an array w/ the `*` char
+# `eval` evaluates the whole string
--- day 3/day3b.rb
+++ day 3/day3b.rb
@@ -1,25 +1,28 @@
-# data = File.read("day3b.test.input")
-data = File.read("day3.input")
+# # data = File.read("day3b.test.input")
+# data = File.read("day3.input")
 
-def mul(str)
-  m = str.match(/mul\((\d+),(\d+)\)/)
-  m[1].to_i * m[2].to_i
-end
+# def mul(str)
+#   m = str.match(/mul\((\d+),(\d+)\)/)
+#   m[1].to_i * m[2].to_i
+# end
 
-re = /mul\(\d+,\d+\)|do\(\)|don\'t\(\)/
+# re = /mul\(\d+,\d+\)|do\(\)|don\'t\(\)/
 
-sum = 0
-enabled = true
-data.scan(re).each do |m|
-  pp "-- #{m}"
-  case m
-  when "don't()"
-    enabled = false
-  when "do()"
-    enabled = true
-  else
-    sum += mul(m) if enabled
-  end
-end
+# sum = 0
+# enabled = true
+# data.scan(re).each do |m|
+#   pp "-- #{m}"
+#   case m
+#   when "don't()"
+#     enabled = false
+#   when "do()"
+#     enabled = true
+#   else
+#     sum += mul(m) if enabled
+#   end
+# end
 
-pp "sum: #{sum}"
+# pp "sum: #{sum}"
+# pp "-----------"
+
+a=1;$<.read.scan(/mul\(\d+,\d+\)|do\(\)|don\'t\(\)/).map{_1=="do()"?a=1:(_1=="don't()"?a=0:(a==1?$*<<eval(_1.scan(/\d+/)*?*):0))};p $*.sum
